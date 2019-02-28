dbe667103f78c851e0f9d3cb3970af09fbcc2643
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-02-28 20:04:23 +0100

    Add className

--- src/init.php
+++ src/init.php
@@ -30,6 +30,9 @@ register_block_type('bonseo/' . $block,
 			),
 			'url' => array(
 				'type' => 'string',
+			),
+			'className' => array(
+				'type' => 'string'
 			)
 
 		),
@@ -86,13 +89,14 @@ function bs_basic_banner_editor_assets()
 
 function render_bs_basic_banner($attributes)
 {
-	$title = $attributes['title'];
-	$content = $attributes['content'];
-	$cta = $attributes['cta'];
-	$url = $attributes['url'];
+	$class = isset($attributes['className']) ? ' ' . $attributes['className'] : '';
+	$title = isset($attributes['title']) ? $attributes['title'] : '';
+	$content = isset($attributes['content']) ? $attributes['content'] : '';
+	$cta = isset($attributes['cta']) ? $attributes['cta'] : '';
+	$url = isset($attributes['url']) ? $attributes['url'] : '';
 
 	return '
-		<section class="og-banner-basic l-flex l-flex--justify-space-around a-bg a-pad l-flex--wrap">
+		<section class="og-banner-basic l-flex l-flex--justify-space-around a-bg a-pad l-flex--wrap' . $class . '">
 			<div class="og-banner-basic-content l-flex l-flex--direction-column a-pad-20">
 				<h2 class="a-text a-text--xl a-text--secondary ">
 					' . $title . '
